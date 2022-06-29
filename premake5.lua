0af9aa2db950309199915f2e7d742deaaf802b03
Joshua Palmer <joshua.after@gmail.com>
2022-06-29 11:11:00 -0600

    update premake file to fix AgaveEngine build integration problems

--- premake5.lua
+++ premake5.lua
@@ -9,12 +9,23 @@ project "GLFW"
         "include/glfw/glfw3.h",
         "include/glfw/glfw3native.h",
         "src/glfw_config.h",
+        "src/internal.h",
+        "src/mappings.h",
+        "src/module.h",
+        "src/platform.h",
+        "src/platform.c",
         "src/context.c",
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
         "src/vulkan.c",
-        "src/window.c"
+        "src/window.c",
+        "src/null_platform.h",
+        "src/null_joystick.h",
+        "src/null_init.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/null_joystick.c"
     }
 
     filter "system:windows"
@@ -25,6 +36,7 @@ project "GLFW"
             "src/win32_init.c",
             "src/win32_joystick.c",
             "src/win32_monitor.c",
+            "src/win32_module.c",
             "src/win32_time.c",
             "src/win32_thread.c",
             "src/win32_window.c",
